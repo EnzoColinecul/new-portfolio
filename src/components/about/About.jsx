5ecd920b4f79c6df59efd29c1ca11fee9e371829
Enzo Colinecul <enzocolinecul@MAC-66632.local>
2022-03-31 23:10:27 -0300

    change image header

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,37 @@
 import React from 'react';
+
+import ME from '../../assets/images/me2.jpeg';
 import './about.css';
 
 function About() {
   return (
-    <section id="about">About</section>
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="AboutImage" />
+          </div>
+        </div>
+        <div className="about__content">
+          <p className="about__p">
+            Me apasiona la tecnología, tengo 24 años y vivo en Esquel - Chubut;
+            Técnico Superior en Sistemas de Telecomunicaciones.
+            <br />
+            Actualmente me desarrollo en el área de Seguridad informática,
+            en el equipo de IAM ( Identity and Access Managment ).
+            <br />
+            Me gustan mucho los desafíos y cumplir objetivos.
+            Tengo un nivel internmedio de Ingles oral y escrito.
+            Aprendo rápido, me encanta trabajar en equipo y me adapto rápido a los cambios.
+            Tengo conocimientos en:
+            - Sistemas / Networking / Telefonia IP / Seguridad Informatica/ Programacion
+          </p>
+          <a href="#contact" className="btn btn-primary">Contact Me</a>
+        </div>
+      </div>
+    </section>
   );
 }
 
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { AiOutlineHome, AiOutlineUser } from 'react-icons/ai';
 import { RiContactsBook2Line, RiServiceLine } from 'react-icons/ri';
@@ -7,21 +7,22 @@ import { BiBook } from 'react-icons/bi';
 import './nav.css';
 
 function Nav() {
+  const [active, setActive] = useState('#home');
   return (
     <nav>
-      <a href="#home">
+      <a href="#home" onClick={() => setActive('#home')} className={active === '#home' ? 'active' : ''}>
         <AiOutlineHome />
       </a>
-      <a href="#about">
+      <a href="#about" onClick={() => setActive('#about')} className={active === '#about' ? 'active' : ''}>
         <AiOutlineUser />
       </a>
-      <a href="#experience">
+      <a href="#experience" onClick={() => setActive('#experience')} className={active === '#experience' ? 'active' : ''}>
         <BiBook />
       </a>
-      <a href="#services">
+      <a href="#services" onClick={() => setActive('#services')} className={active === '#services' ? 'active' : ''}>
         <RiServiceLine />
       </a>
-      <a href="#contact">
+      <a href="#contact" onClick={() => setActive('#contact')} className={active === '#contact' ? 'active' : ''}>
         <RiContactsBook2Line />
       </a>
     </nav>
