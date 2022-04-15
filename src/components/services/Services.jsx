78565a13150ada31f23746690d059768765246b6
Enzo Colinecul <enzocolinecul@MAC-66632.fibertel.com.ar>
2022-04-15 20:30:42 -0300

    ADD 2 articles

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,11 +1,62 @@
 import React from 'react';
+import { HiBadgeCheck } from 'react-icons/hi';
+
+import './services.css';
 
 function Services() {
   return (
     <section id="services">
       <h5>What I Offer</h5>
       <h2>Services</h2>
-      <div className="container services__container" />
+      <div className="container services__container">
+        <article className="service">
+          <div className="service__head">
+            <h3>Web Development</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <HiBadgeCheck className="service__icon" />
+              <p>lorem ipsum</p>
+            </li>
+            <li>
+              <HiBadgeCheck className="service__icon" />
+              <p>lorem ipsum</p>
+            </li>
+            <li>
+              <HiBadgeCheck className="service__icon" />
+              <p>lorem ipsum</p>
+            </li>
+            <li>
+              <HiBadgeCheck className="service__icon" />
+              <p>lorem ipsum</p>
+            </li>
+            <li>
+              <HiBadgeCheck className="service__icon" />
+              <p>lorem ipsum</p>
+            </li>
+          </ul>
+        </article>
+        {/* Cybersecurity */}
+        <article className="service">
+          <div className="service__head">
+            <h3>Cybersecurity - IAM </h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <HiBadgeCheck className="service__icon" />
+              <p>lorem ipsum</p>
+            </li>
+            <li>
+              <HiBadgeCheck className="service__icon" />
+              <p>lorem ipsum</p>
+            </li>
+            <li>
+              <HiBadgeCheck className="service__icon" />
+              <p>lorem ipsum</p>
+            </li>
+          </ul>
+        </article>
+      </div>
     </section>
   );
 }
